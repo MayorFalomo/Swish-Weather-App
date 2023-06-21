d1c7a37db7f47b46e31bf544464158a8a64291ff
MayorFalomo <mayorfalomo@gmail.com>
2023-06-21 16:13:09 +0100

    fixed the navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,109 +1,3 @@
-// import React, { useContext } from "react";
-// import { ImSearch } from "react-icons/im";
-// import { FiSun } from "react-icons/fi";
-// import { StyledNav } from "./Nav.styled";
-// import { Menubar } from "./Nav.styled";
-// import { Searchbar } from "./Nav.styled";
-// import { AppContext } from "../Helper/Context";
-// import { BsFillMoonFill } from "react-icons/bs";
-// import CountriesList from "../CountriesList/CountriesList";
-
-// export const Navbar = () => {
-//   const {
-//     searchInput,
-//     setSearchInput,
-//     fetchWeather,
-//     theme,
-//     setTheme,
-//     fetchCountry,
-//   } = useContext(AppContext);
-
-//   const [pressed, setPressed] = React.useState(false);
-//   const [searchText, setSearchText] = React.useState("Nigeria");
-
-//   // Send input value
-//   const handleChange = (e: any) => {
-//     setSearchInput(e.target.value);
-//     setSearchText(e.target.value);
-//   };
-
-//   //Submit input value
-//   const handleSubmit = (e: any) => {
-//     e.preventDefault();
-//     fetchWeather(searchInput);
-//     fetchCountry(searchInput);
-//     setPressed(false);
-//   };
-
-//   //Handle Lighting
-//   const toggleTheme = () => {
-//     theme === "light" ? setTheme("dark") : setTheme("light");
-//   };
-
-//   return (
-//     <StyledNav theme={theme}>
-//       <nav>
-//         <h2>SWISH. </h2>
-//         <Menubar>
-//           <Searchbar>
-//             <form
-//               style={{
-//                 display: "flex",
-//                 alignItems: "center",
-//                 width: "300px",
-//                 overflow: "hidden",
-//                 gap: "0.5rem",
-//               }}
-//               onSubmit={(e) => handleSubmit(e)}
-//             >
-//               <input
-//                 value={searchText}
-//                 onChange={handleChange}
-//                 onClickCapture={() => setPressed(true)}
-//                 placeholder="Enter The Country..."
-//                 type="text"
-//                 required
-//                 style={{
-//                   width: "90%",
-//                   textTransform: "capitalize",
-//                 }}
-//               />
-//               <button
-//                 // onClick={(e) => handleSubmit(e)}
-//                 style={{
-//                   display: "grid",
-//                   placeContent: "center",
-//                   padding: "0.5rem",
-//                   cursor: "pointer",
-//                 }}
-//               >
-//                 <ImSearch className="search-ico" style={{ fontSize: "1.2rem", fontWeight: "800" }} />
-//               </button>
-//             </form>
-//             {searchText?.length !== 0 && (
-//               <CountriesList
-//                 searchText={searchText}
-//                 setSearchText={setSearchText}
-//                 setSearchInput={setSearchInput}
-//                 setPressed={setPressed}
-//               />
-//             )}
-//           </Searchbar>
-//           {theme === "dark" ? (
-//             <p onClick={() => toggleTheme()} style={{ cursor: "pointer" }}>
-//               <BsFillMoonFill />
-//             </p>
-//           ) : (
-//             <p onClick={() => toggleTheme()} style={{ cursor: "pointer" }}>
-//               <FiSun />
-//             </p>
-//           )}
-//         </Menubar>
-//       </nav>
-//     </StyledNav>
-//   );
-// }
-// };
 import React, { useContext, } from "react";
 import { CiSearch } from "react-icons/ci";
 import { FiSun } from "react-icons/fi";
@@ -117,7 +11,7 @@ import CountriesList from "../CountriesList/CountriesList";
 
 export const Navbar = () => {
 
-  const { searchInput, setSearchInput,country,setCountry, fetchWeather, theme, setTheme, currentUnit, setCurrentUnit } =
+  const { searchInput, setSearchInput,country, fetchWeather, theme, setTheme, currentUnit, setCurrentUnit } =
     useContext(AppContext);
 
   const [searchText, setSearchText] = React.useState("Nigeria");
